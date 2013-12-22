462626fe56702029f460272eae93e4e5808bbd81
Achim Rolle <rolle@hucke-media.de>
2013-12-22 15:55:04 +0100

    change design, add js namespace

--- skin/frontend/base/default/js/hauptrolle_toolbox.js
+++ skin/frontend/base/default/js/hauptrolle_toolbox.js
@@ -1,9 +1,35 @@
+var hauptrolle = {
+    showToolbox : function(){
+        $$('#hauptrolle__toolbox')[0].setStyle({
+            left: -250+'px'
+        });
+    },
+
+    hideToolbox : function() {
+        $$('#hauptrolle__toolbox')[0].setStyle({
+            left: 0
+        });
+    }
+}
+
+
 document.observe('dom:loaded', function() {
 
-    // Center Toolbox vertical
-    var toolboxHeight = $$('#hauptrolle__toolbox')[0].getHeight();
-    $$('#hauptrolle__toolbox')[0].setStyle({
-        marginTop: '-' + toolboxHeight / 2 +'px'
+    // Toggle Toolbox
+
+    if(Mage.Cookies.get('toolbox_status') == "visible") {
+        
+    }
+
+    $('hauptrolle__toogle').observe('click', function(event) {
+        event.preventDefault();
+        var leftCSS = $$('#hauptrolle__toolbox')[0].getStyle('left');
+        if(leftCSS == "0px") {
+            hauptrolle.showToolbox();
+        }
+        else {
+            hauptrolle.hideToolbox();
+        }
     });
 
     // Check if jQuery is loaded
