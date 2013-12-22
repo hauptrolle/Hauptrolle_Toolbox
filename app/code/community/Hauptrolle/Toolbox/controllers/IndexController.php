fca6931c1d955dd38490c3e844795bd187c1c5ee
Achim Rolle <rolle@hucke-media.de>
2013-12-22 01:15:44 +0100

    add toggle template hints

--- app/code/community/Hauptrolle/Toolbox/controllers/IndexController.php
+++ app/code/community/Hauptrolle/Toolbox/controllers/IndexController.php
@@ -8,4 +8,20 @@ class Hauptrolle_Toolbox_IndexController extends Mage_Core_Controller_Front_Acti
         $this->_redirectReferer();
     }
 
+    public function toggleTemplateHintsAction()
+    {
+        if(Mage::getStoreConfig('dev/debug/template_hints') == 0) {
+            $write = Mage::getSingleton('core/resource')->getConnection('core_write');
+            $write->query("UPDATE core_config_data SET value = 1 WHERE scope='websites' AND scope_id = 1 AND path ='dev/debug/template_hints'");
+            Mage::app()->getCacheInstance()->flush();
+            $this->_redirectReferer();
+        }
+        else {
+            $write = Mage::getSingleton('core/resource')->getConnection('core_write');
+            $write->query("UPDATE core_config_data SET value = 0 WHERE scope='websites' AND scope_id = 1 AND path ='dev/debug/template_hints'");
+            Mage::app()->getCacheInstance()->flush();
+            $this->_redirectReferer();
+        }
+
+    }
 }
