0563642af9fb6d13683bfb858fc498abd626d537
Achim Rolle <info@achimrolle.de>
2014-07-01 17:25:43 +0200

    add store id to template hints toggle

--- app/code/community/Hauptrolle/Toolbox/controllers/ToolController.php
+++ app/code/community/Hauptrolle/Toolbox/controllers/ToolController.php
@@ -10,11 +10,14 @@ class Hauptrolle_Toolbox_ToolController extends Mage_Core_Controller_Front_Actio
 
     public function toggleTemplateHintsAction()
     {
+
+        $storeId =  Mage::app()->getStore()->getStoreId();
+
         if(Mage::getStoreConfig('dev/debug/template_hints') == 0) {
-            Mage::getModel('core/config')->saveConfig('dev/debug/template_hints', 1, 'stores', 1);
+            Mage::getModel('core/config')->saveConfig('dev/debug/template_hints', 1, 'stores', $storeId);
         }
         else {
-            Mage::getModel('core/config')->saveConfig('dev/debug/template_hints', 0, 'stores', 1);
+            Mage::getModel('core/config')->saveConfig('dev/debug/template_hints', 0, 'stores', $storeId);
         }
         Mage::getConfig()->cleanCache();
         $this->_redirectReferer();
